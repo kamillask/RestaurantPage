3b84d5d6680d4ab22fff410442ef7bbaaad7a598
kamillask <kamil.laskowski86@qmail.cuny.edu>
2025-04-01 19:22:12 -0400

    Added functionality to display menu, clear content, CSS updates.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,24 @@ const homeButton = document.querySelector("#home");
 const menuButton = document.querySelector("#menu");
 const aboutButton = document.querySelector("#about");
 
+const content = document.getElementById("content");
+function clearContent() {
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+}
+
 homeButton.addEventListener("click", () => {
+    clearContent();
     showHome();
 });
 
 menuButton.addEventListener("click", () => {
+    clearContent();
     showMenu();
 });
 
 aboutButton.addEventListener("click", () => {
+    clearContent();
     showAbout();
 });
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,48 @@
+class Menu {
+    constructor(name, description, price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+
+let vesuvioMenu = [];
+
+let quail = new Menu("Quail, a la Bucco", "Baby quail stuffed with fennel sausage.", "Priceless");
+vesuvioMenu.push(quail);
+let gabagool = new Menu("Gabagool", "Gabagool? Over hereeeee!", "$4/lb");
+vesuvioMenu.push(gabagool);
+let ziti = new Menu("Ziti", "So what, no flippin ziti now? Oh!", "idk");
+vesuvioMenu.push(ziti);
+
+function createMenuItemDiv(menuItem){
+    const menuItemContainer = document.createElement("div");
+
+    const itemName = document.createElement("div");
+    itemName.classList.add("menuItemName");
+    itemName.textContent = menuItem.name;
+    menuItemContainer.appendChild(itemName);
+
+    const itemDescription = document.createElement("div");
+    itemDescription.classList.add("menuItemDescription");
+    itemDescription.textContent = menuItem.description;
+    menuItemContainer.appendChild(itemDescription);
+
+    const itemPrice = document.createElement("div");
+    itemPrice.classList.add("menuItemPrice");
+    itemPrice.textContent = menuItem.price;
+    menuItemContainer.appendChild(itemPrice);
+
+    return menuItemContainer;
+}
+
+const contentDiv = document.getElementById("content");
+
+
 export function showMenu(){
-    alert("menu clicked")
+    for(let item in vesuvioMenu){
+        contentDiv.appendChild(createMenuItemDiv(vesuvioMenu[item]));
+    }
+    
 }
